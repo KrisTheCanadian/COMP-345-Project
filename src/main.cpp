ac92ac40610b40fd47b716f16df4db725ff1dab4
Raisa Zaman <raisazaman2000@gmail.com>
2022-10-01 20:11:52 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
+#include "MapDriver.h"
+#include "GameEngineDriver.h"
 #include "OrdersDriver.h"
 
 using namespace std;
 
 int main()
 {
+    testLoadMaps();
+    testGameStates();
     testOrdersLists();
     return 0;
 }
