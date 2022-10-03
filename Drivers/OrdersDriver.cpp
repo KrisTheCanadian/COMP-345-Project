8c5905b48b57b5809890d0d093d99c5c3e855b88
Raisa Zaman <raisazaman2000@gmail.com>
2022-10-03 13:12:34 -0400

    Remove commented out code

--- Drivers/OrdersDriver.cpp
+++ Drivers/OrdersDriver.cpp
@@ -17,35 +17,6 @@ void testOrdersLists()
   list.move(4, 2);
   list.remove(2);
 
-  //   cout << "-> Copy constructor copying the list :";
-  //   ordersList CopyConstList = ordersList(list);
-  //   cout << CopyConstList;
-
-  //   cout << "-> Assignment operator copying the list"<< endl;
-  //   ordersList oL;
-  //   oL.add(userinputorder.create("deploy"));
-  //   oL.add(userinputorder.create("bomb"));
-  //   oL = list;
-  //   cout << oL;
-
   cout << "-> List order execution" << endl;
   list.orderExecuter();
-
-  //   cout << "-> Cout the lists" << endl;
-
-  //   cout << "The initial list: "<< list << endl;
-  //   cout << "Constructor list: "<< CopyConstList << endl;
-  //   cout << "Assignment operator list: "<< oL << endl;
-
-  //   cout << "-> Cout copied lists and operations:";
-
-  //   list.add(userinputorder.create("deploy"));
-  //   list.add(userinputorder.create("airlift"));
-  //   CopyConstList.remove(2);
-  //   cout << "-> Cout the original list:\n"
-  //       << list << endl;
-  //   cout << "-> Cout the copy constructor list:\n"
-  //       << CopyConstList << endl;
-  //   cout << "-> Cout assignment operator list:\n"
-  //       << oL << endl;
 }
