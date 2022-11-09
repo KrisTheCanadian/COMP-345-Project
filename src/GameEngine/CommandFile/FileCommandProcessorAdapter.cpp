bb27d867448e53e29d7a69663848077801e49556
Zafir Khalid <zafirmk0@gmail.com>
2022-11-09 00:28:56 -0500

    Part 1: Command Line Implementation + File Implementation (#24)
    * Command line functionality for part 1
    
    * CLI Implementation of Part 1 (Not with GameEngine)
    
    * Stream insertion & assignment for CommandProcessor
    
    * Uncomment main.cpp
    
    * Adding sample command files for testing
    
    * Add test file
    
    * Add command file processing stuff
    
    * Update existing command processor stuff
    
    * Update main
    
    * fixed build error + refactored
    
    * Changed currentLine from globalscope to classscope
    
    * Update test file
    
    * Remove cout's
    
    Co-authored-by: Kris <kristhecanadian101@gmail.com>

---
+++ src/GameEngine/CommandFile/FileCommandProcessorAdapter.cpp
@@ -0,0 +1,31 @@
+#include "FileCommandProcessorAdapter.h"
+#include <iostream>
+
+void FileCommandProcessorAdapter::commandLineToFile(FileLineReader* _flr){
+    flr = _flr;
+}
+
+std::string FileCommandProcessorAdapter::readCommand(){
+    return flr->readLineFromFile();
+}
+
+FileCommandProcessorAdapter::FileCommandProcessorAdapter(){}
+
+FileCommandProcessorAdapter::FileCommandProcessorAdapter(const FileCommandProcessorAdapter &fcpA){
+    flr = fcpA.flr;
+}
+
+std::ostream & operator << (std::ostream &out, const FileCommandProcessorAdapter &fcpA){
+    out << "Adapter connected to: " << fcpA.flr << std::endl;
+    return out;
+}
+
+FileCommandProcessorAdapter& FileCommandProcessorAdapter::operator=(const FileCommandProcessorAdapter& other){
+    if(this == &other){
+        return *this;
+    }
+
+    this->flr = other.flr;
+
+    return *this;
+}
---
+++ test/Command/TEST_Command.cpp
@@ -0,0 +1,96 @@
+#include <memory>
+#include <gtest/gtest.h>
+#include "GameEngine/CommandFile/FileCommandProcessorAdapter.h"
+#include "GameEngine/CommandFile/FileLineReader.h"
+
+TEST(FileCommandTestSuite, CommandFile2_invalid){
+
+    int final_state;
+
+    FileLineReader flr;
+    FileCommandProcessorAdapter adapter;
+    adapter.commandLineToFile(&flr);
+    flr.setFile("../res/TestCommand2_invalid.txt");
+
+    while(!flr.getReadComplete()){
+        adapter.getCommand();
+    }
+
+    final_state = adapter.getCurrentState();
+
+
+    EXPECT_FALSE(final_state == 4);
+}
+
+TEST(FileCommandTestSuite, CommandFile3_invalid){
+
+    FileLineReader flr;
+    FileCommandProcessorAdapter adapter;
+    adapter.commandLineToFile(&flr);
+    flr.setFile("../res/TestCommand3_invalid.txt");
+
+    while(!flr.getReadComplete()){
+        adapter.getCommand();
+    }
+
+    int final_state = adapter.getCurrentState();
+
+    EXPECT_FALSE(final_state == 4);
+}
+
+
+
+TEST(FileCommandTestSuite, CommandFile5_invalid){
+
+    FileLineReader flr;
+    FileCommandProcessorAdapter adapter;
+    adapter.commandLineToFile(&flr);
+    flr.setFile("../res/TestCommand5_invalid_format.txt");
+
+    while(!flr.getReadComplete()){
+        adapter.getCommand();
+    }
+
+    int final_state = adapter.getCurrentState();
+    EXPECT_FALSE(final_state == 4);
+}
+
+
+TEST(FileCommandTestSuite, CommandFile1_Valid){
+
+    FileLineReader flr;
+    FileCommandProcessorAdapter adapter;
+    adapter.commandLineToFile(&flr);
+    flr.setFile("../res/TestCommand1_valid.txt");
+
+    while(!flr.getReadComplete()){
+        adapter.getCommand();
+    }
+
+    int final_state = adapter.getCurrentState();
+
+    EXPECT_TRUE(final_state == 4);
+}
+
+TEST(FileCommandTestSuite, CommandFile4_valid){
+
+    FileLineReader flr;
+    FileCommandProcessorAdapter adapter;
+    adapter.commandLineToFile(&flr);
+    flr.setFile("../res/TestCommand4_valid.txt");
+
+    while(!flr.getReadComplete()){
+        adapter.getCommand();
+    }
+
+    int final_state = adapter.getCurrentState();
+
+    EXPECT_TRUE(final_state == 4);
+}
+
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
