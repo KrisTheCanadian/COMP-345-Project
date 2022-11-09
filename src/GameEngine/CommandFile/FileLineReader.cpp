671b7871136ca55bfde11fad12797d364913b39d
Zafirmk <zafirmk0@gmail.com>
2022-11-08 20:43:39 -0500

    Add command file processing stuff

---
+++ src/GameEngine/CommandFile/FileLineReader.cpp
@@ -0,0 +1,76 @@
+#include "FileLineReader.h"
+#include <fstream>
+#include <iostream>
+
+int currentLine = 1;
+
+FileLineReader::FileLineReader(){}
+
+FileLineReader::FileLineReader(const FileLineReader &flr){
+    fileName = flr.fileName;
+    readComplete = flr.readComplete;
+}
+
+std::string FileLineReader::readLineFromFile(){
+
+    std::ifstream fileInputStream(fileName);
+    int maxLines = maxNumberOfLines(fileName);
+    std::string line;
+    
+    if (fileInputStream.is_open() && !readComplete){
+      GotoLine(fileInputStream, currentLine);
+      getline(fileInputStream, line);
+      if (currentLine == maxLines){
+        this->readComplete = true;
+      }
+      currentLine++;
+      return line;
+    } else {
+      throw std::runtime_error("Could not open file");
+    }
+    
+    return "";
+}
+
+std::ifstream& FileLineReader::GotoLine(std::ifstream& file, unsigned int num){
+    file.seekg(std::ios::beg);
+    for(int i=0; i < num - 1; ++i){
+        file.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    }
+    return file;
+}
+
+int FileLineReader::maxNumberOfLines(std::string _fileName){
+  std::ifstream temp(_fileName);
+  int lines_count = 0;
+  std::string line;
+  while (std::getline(temp , line)){
+    lines_count++;
+  }
+  return lines_count;
+}
+
+void FileLineReader::setFile(std::string _fileName){
+  fileName = _fileName;
+}
+
+bool FileLineReader::getReadComplete(){
+  return this->readComplete;
+}
+
+std::ostream & operator << (std::ostream &out, const FileLineReader &flr)
+{
+    out << "Current file: " << flr.fileName << std::endl;
+    return out;
+}
+
+FileLineReader& FileLineReader::operator=(const FileLineReader &other) {
+  if(this == &other){
+    return *this;
+  }
+
+  this->fileName = other.fileName;
+  this->readComplete = other.readComplete;
+
+  return *this;
+}
---
+++ src/GameEngine/CommandFile/FileLineReader.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+
+class FileLineReader{
+
+    private:
+        std::string fileName;
+        bool readComplete = false;
+
+    public:
+    
+        // Default constructor
+        FileLineReader();
+
+        // Copy constructor
+        FileLineReader(const FileLineReader &flr);
+
+        // Reading line from file
+        virtual std::string readLineFromFile();
+        
+        // Jump to num line number
+        std::ifstream& GotoLine(std::ifstream& file, unsigned int num);
+        
+        // Return maximum number of lines in _fileName
+        int maxNumberOfLines(std::string _fileName);
+        
+        // Relevant getters and setters
+        void setFile(std::string _fileName);
+        bool getReadComplete();
+
+        //Operator Overloading
+        friend std::ostream & operator << (std::ostream &out, const FileLineReader &flr);
+        FileLineReader& operator=(const FileLineReader& other);
+};
